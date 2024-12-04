83f1dd0ed65dfe1c183f0d9536126d09e237dc41
Ekstrapolator <wojciech.wolek@gmail.com>
2024-12-04 22:35:17 +0100

    day_02 update

--- day_02/main.cpp
+++ day_02/main.cpp
@@ -1,15 +1,15 @@
 #include "fmt/format.h"
 #include "InputManager.hpp"
 #include "Tokenizer.hpp"
-#include "../outputBuilder/OutputBuilder.hpp"
+#include "OutputBuilder.hpp"
 
 int main(int argv, const char** argc)
 {
   Tokenizer tokenizer;
-  InputManager inputOne("day_02/input_01.data");
+  InputManager inputOne("/home/wolek/CODE/cppCode/AoC_2024/day_02/input_1_test.data");
   auto input = inputOne.getInput();
   auto lines = tokenizer.parse(input, "\n");
   OutputBuilder output;
-  output.getDay02_part01(lines);
-
+  safeRaports safe = output.getDay02_part01(lines);
+  fmt::print("Safe reports count {}", safe);
 }
--- outputBuilder/OutputBuilder.cpp
+++ outputBuilder/OutputBuilder.cpp
@@ -36,7 +36,7 @@ size_t OutputBuilder::getDay01_part01(std::vector<std::string> &tokens) {
   std::vector<size_t> leftList;
   std::vector<size_t> rightList;
 
-  for (auto& token : tokens) {
+  for (auto &token : tokens) {
     std::string snumberOne;
     std::string snumberTwo;
     bool firstNumber{true};
@@ -65,27 +65,23 @@ size_t OutputBuilder::getDay01_part01(std::vector<std::string> &tokens) {
 
   assert(leftList.size() == rightList.size() && "List must have same size");
 
-  for(auto i{0}; i < leftList.size(); i++)
-  {
-    if(leftList.at(i) < rightList.at(i))
-    {
+  for (auto i{0}; i < leftList.size(); i++) {
+    if (leftList.at(i) < rightList.at(i)) {
       distance.push_back(rightList.at(i) - leftList.at(i));
-    }
-    else{
+    } else {
       distance.push_back(leftList.at(i) - rightList.at(i));
     }
 
-
   }
 
-  return std::accumulate(distance.begin(), distance.end(),static_cast<size_t>(0));
+  return std::accumulate(distance.begin(), distance.end(), static_cast<size_t>(0));
 }
 
 size_t OutputBuilder::getDay01_part02(std::vector<std::string> &tokens) {
   std::vector<size_t> leftList;
   std::vector<size_t> rightList;
 
-  for (auto& token : tokens) {
+  for (auto &token : tokens) {
     std::string snumberOne;
     std::string snumberTwo;
     bool firstNumber{true};
@@ -108,46 +104,58 @@ size_t OutputBuilder::getDay01_part02(std::vector<std::string> &tokens) {
 
   assert(leftList.size() == rightList.size() && "List must have same size");
   size_t resoult{0};
-  for(auto num : leftList)
-  {
+  for (auto num : leftList) {
     size_t count = std::count(rightList.begin(), rightList.end(), num);
     resoult += count * num;
   }
   return resoult;
 }
 
-
-
 safeRaports OutputBuilder::getDay02_part01(std::vector<std::string> tokens) {
   safeRaports count{0};
-  for (auto tok : tokens)
-  {
+  for (auto tok : tokens) {
     std::string num;
     std::vector<int> raport;
-    for (const auto& c : tok)
-    {
-      if (c == ' ')
-      {
+    for (const auto &c : tok) {
+      if (c == ' ') {
         raport.push_back(std::stoi(num));
         num.clear();
-      }
-      else{
-      num.push_back(c);
+      } else {
+        num.push_back(c);
       }
     }
-    if (isRaportSafe(raport))
-    {
+    if (isRaportSafe(raport)) {
       count++;
-    }
-    else
-    {
+    } else {
       fmt::print("Unsafe raport\n");
     }
   }
-return 0;
+  return count;
 }
 
-bool OutputBuilder::isRaportSafe(std::vector<int>& inRaport) {
+bool OutputBuilder::isRaportSafe(std::vector<int> &inRaport) {
+  std::vector<int> ascendig = inRaport;
+  std::vector<int> descndig = inRaport;
+  std::sort(ascendig.begin(), ascendig.end(), std::less<>());
+  std::sort(descndig.begin(), descndig.end(), std::greater<>());
+  if (ascendig != inRaport and descndig != inRaport) {
+    return false;
+  }
 
-return 0;
+  if (ascendig == inRaport) {
+    for (size_t i{1}; i < inRaport.size(); i++) {
+      if (abs(inRaport.at(i) - inRaport.at(i - 1)) > 3 or abs(inRaport.at(i) - inRaport.at(i - 1)) == 0) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    // descending by 1 or 2
+    for (size_t i{1}; i < inRaport.size(); i++) {
+      if (abs(inRaport.at(i) - inRaport.at(i - 1)) > 3 or abs(inRaport.at(i) - inRaport.at(i - 1)) == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
