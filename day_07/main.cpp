3e7b17427fd39a02e12f6193aa656b947c67bb42
wolek <wojciech.wolek@nokia.com>
2025-05-07 10:18:27 +0200

    working day 07 part 01

---
+++ day_07/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by wolek on 23.04.25.
+//
+#include "fmt/format.h"
+#include "InputManager.hpp"
+#include "Tokenizer.hpp"
+#include "OutputBuilder.hpp"
+#include <filesystem>
+#include <regex>
+
+int main(int argv, const char** argc)
+{
+  auto path = std::filesystem::current_path();
+  fmt::print("Current path: {}\n", path.c_str());
+
+  Tokenizer tokenizer;
+  InputManager inputOne("day_06/input_01.data");
+  auto puzzleArray = inputOne.getStaticInput();
+
+
+  OutputBuilder output;
+
+}
